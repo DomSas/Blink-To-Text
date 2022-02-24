574886b2f0814435ce180699dca431a552da7242
Dominik Sasko <62658199+DomSas@users.noreply.github.com>
2022-02-24 07:50:03 +0100

    Added Pinia store with logic to the app. (#6)

--- src/pages/PredictingPage.vue
+++ src/pages/PredictingPage.vue
@@ -2,54 +2,108 @@
   <f7-page name="PredictingPage">
     <div class="predicting-page">
       <h3 class="predicting-page__title">Blink-To-Text</h3>
-      <p class="predicting-page__description">Use <a href="/morse-code">Morse Code</a> method: <br/> Short blink - dot <br/> Long blink - dash</p>
-      <video class="predicting-page__video" id="predicting-page__video" playsinline></video>
+      <p class="predicting-page__description">
+        Use <a href="/morse-code">Morse Code</a> method: <br />
+        Short blink - dot <br />
+        Long blink - dash
+      </p>
+      <video
+        class="predicting-page__video"
+        id="predicting-page__video"
+        playsinline
+      ></video>
 
       <p class="predicting-page__recognized-blink-title">Recognized blink:</p>
       <div class="predicting-page__recognized-blink-container">
-
+        <p
+          class="predicting-page__recognized-blink-sequence"
+          v-for="(sign, index) in blinkStore.blinkSequence"
+          :key="index"
+        >
+          {{ sign }}
+        </p>
       </div>
 
       <p class="predicting-page__recognized-text-title">Recognized text:</p>
       <div class="predicting-page__recognized-text-container">
-        <p class="predicting-page__recognized-text-p"></p>
+        <p class="predicting-page__recognized-text-p">
+          {{ blinkStore.convertedLetters }}
+        </p>
       </div>
 
       <div class="predicting-page__btns-container">
-        <a class="predicting-page__start-capturing-btn">Start Capturing</a>
-        <a class="predicting-page__remove-letter-btn">Remove Letter</a>
+        <a
+          class="predicting-page__start-capturing-btn"
+          v-if="!blinkStore.capturing"
+          @click="startCapturing"
+          :disabled="blinkStore.capturing"
+          >Start Capturing</a
+        >
+        <a
+          class="predicting-page__stop-capturing-btn"
+          v-else
+          @click="stopCapturing"
+          :disabled="blinkStore.capturing"
+          >Stop Capturing</a
+        >
+        <a class="predicting-page__remove-letter-btn" @click="removeLastLetter"
+          >Remove Letter</a
+        >
       </div>
-
     </div>
-
   </f7-page>
 </template>
 
 <script>
+import { onMounted } from 'vue';
+import { useBlinkStore } from '../store/blinkStore';
+
 export default {
-  mounted() {
-    const video = document.getElementById('predicting-page__video');
+  setup() {
+    const blinkStore = useBlinkStore();
 
-    navigator.mediaDevices.getUserMedia({
-      audio: false,
-      video: {
-        facingMode: 'user',
-        width: 500,
-        height: 500,
-      },
-    })
-      .then((stream) => {
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch((err) => {
-        console.log('Something went wrong!', err);
-      });
-  },
-  methods: {
-    playVideo() {
+    const playVideo = () => {
+      const video = document.getElementById('predicting-page__video');
 
-    },
+      navigator.mediaDevices
+        .getUserMedia({
+          audio: false,
+          video: {
+            facingMode: 'user',
+            width: 500,
+            height: 500,
+          },
+        })
+        .then((stream) => {
+          video.srcObject = stream;
+          video.play();
+        })
+        .catch((err) => {
+          console.log('Something went wrong!', err);
+        });
+    };
+
+    const startCapturing = () => {
+      blinkStore.startCapturingBlinks();
+    };
+    const stopCapturing = () => {
+      blinkStore.stopCapturingBlinks();
+    };
+
+    const removeLastLetter = () => {
+      blinkStore.removeLastLetter();
+    };
+
+    onMounted(() => {
+      playVideo();
+    });
+
+    return {
+      blinkStore,
+      startCapturing,
+      stopCapturing,
+      removeLastLetter,
+    };
   },
 };
 </script>
